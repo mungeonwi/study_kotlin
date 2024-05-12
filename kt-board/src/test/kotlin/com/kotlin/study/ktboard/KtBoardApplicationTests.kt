f79e72b5748ac0d61e46a610b8eda14ff2039216
juhwanHeo <juhwanheo.dev@gmail.com>
2024-05-12 13:55:46 +0900

    feat: add kt-board project

---
+++ kt-board/src/test/kotlin/com/kotlin/study/ktboard/KtBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.study.ktboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
