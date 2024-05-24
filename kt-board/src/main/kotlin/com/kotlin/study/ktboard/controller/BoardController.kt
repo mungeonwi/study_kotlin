b956ea614a38d3c4a203bfdfdc28453e15ad846d
mungeon-wi <moongw@healthhub.kr>
2024-05-24 21:15:01 +0900

    feat: add BoardController

--- kt-board/src/main/kotlin/com/kotlin/study/ktboard/controller/BoardController.kt
+++ kt-board/src/main/kotlin/com/kotlin/study/ktboard/controller/BoardController.kt
@@ -1,8 +1,28 @@
 package com.kotlin.study.ktboard.controller
 
+import com.kotlin.study.ktboard.controller.dto.ReqCreateBoardDto
+import com.kotlin.study.ktboard.domain.Board
+import com.kotlin.study.ktboard.service.BoardService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BoardController {
+@RequestMapping("/board")
+class BoardController(
+    private val boardService : BoardService,
+) {
     // @TODO implements
+    @PostMapping
+    fun create(
+        @RequestBody dto: ReqCreateBoardDto,
+    ): ResponseEntity<Board> {
+        return ResponseEntity.ok(boardService.create(dto))
+    }
+
+
 }
