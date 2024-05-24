fe41cf19b8c58a49ad3e7b05a0d902a0fb3e4c04
mungeon-wi <moongw@healthhub.kr>
2024-05-24 21:13:41 +0900

    feat: add board domain

--- kt-board/src/main/kotlin/com/kotlin/study/ktboard/repository/BoardRepository.kt
+++ kt-board/src/main/kotlin/com/kotlin/study/ktboard/repository/BoardRepository.kt
@@ -1,8 +1,10 @@
 package com.kotlin.study.ktboard.repository
 
+import com.kotlin.study.ktboard.domain.Board
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BoardRepository {
+interface BoardRepository : JpaRepository<Board, Long>{
     // @TODO implements
 }
